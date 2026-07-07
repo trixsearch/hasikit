9431cad18fefa5c8dfceb0ea3883d29c8608745e
trixsearch <abhinav.sahu@kiya.ai>
2026-07-07 11:54:50 +0530

    changed min sdk to 26 for supporting upto android 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         applicationId = "com.trixsearch.hasikit"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
